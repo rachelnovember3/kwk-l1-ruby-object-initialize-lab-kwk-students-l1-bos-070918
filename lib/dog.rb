1d1235e1f22c06cb6b4ae1b2493c5b556ce194ac
rachelnovember3 <rachelnovember3@gmail.com>
2018-07-13 14:59:30 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
 # dog.rb
+class Dog
+  
+  def initialize(name)
+    @name = name
+  end
+end
+
+fido = Dog.new("Fido")
+pug = Dog.new("Pug")
+
+
+
+
+# class Person
+  
+#   def initialize(name)
+#     @name = name
+#   end
+# end
+
+# beyonce = Person.new("Beyonce")
